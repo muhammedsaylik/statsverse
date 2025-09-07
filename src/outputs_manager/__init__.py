85d75184fceb63c131b1065726bc80be1f409c78
Muhammed <ilyassaylik25@gmail.com>
2025-09-07 21:05:28 +0300

    ...

--- src/outputs_manager/__init__.py
+++
@@ -1,3 +0,0 @@
-from .manager import OutputManager
-from .r_manager import ROutputManager
-
--- src/outputs_manager/utils.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-def create_module_dirs(base_path):
-    """figures, tables, reports klasörlerini oluşturur"""
-    for folder in ["figures", "tables", "reports"]:
-        (base_path / folder).mkdir(parents=True, exist_ok=True)
-
-
